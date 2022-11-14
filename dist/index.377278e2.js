9cd9ff696ddd2a551a8049ea37b408ab63e48599
smartdev0612 <danieldev0612@gmail.com>
2022-11-14 02:34:48 -0800

    initial commit

---
+++ dist/index.377278e2.js
@@ -0,0 +1,3 @@
+console.log("Hi there!");
+
+//# sourceMappingURL=index.377278e2.js.map
---
+++ src/User.ts
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker';
+
+class User {
+  name: string;
+  localhost: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    
+  }
+}
