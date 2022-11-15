4017960e5d6290dbe363d27e09c7c392584ea71b
smartdev0612 <danieldev0612@gmail.com>
2022-11-14 18:44:56 -0800

    add google map

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from './User';
 import { Company } from './Company';
 
@@ -8,3 +10,11 @@ console.log(user);
 const company = new Company();
 
 console.log(company);
+
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
